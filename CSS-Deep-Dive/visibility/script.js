e7eab30558827ec3e1aea6ead108e5a6d49f62e0
RUDYGAMES <rudygamesv2@gmail.com>
2023-08-17 10:46:27 +0200

    change conception project, added btns

--- CSS-Deep-Dive/visibility/script.js
+++ CSS-Deep-Dive/visibility/script.js
@@ -1,6 +1,23 @@
+const btns = document.querySelectorAll('.btn')
+
+const showHideBox = e => {
+	const value = e.target.value
+	const box = e.target.previousElementSibling.previousElementSibling
+
+	if (e.target.value == 'visibility: hidden')
+		box.style.visibility === 'hidden' ? (box.style = 'visibility: visible;') : (box.style = value)
+	else if (e.target.value == 'display: none')
+		box.style.display === 'none' ? (box.style = 'display: flex') : (box.style = value)
+	else if (e.target.value == 'opacity: 0') box.style.opacity === '0' ? (box.style = 'opacity: 1') : (box.style = value)
+}
+
+btns.forEach(btn => {
+	btn.addEventListener('click', showHideBox)
+})
+
 const boxes = document.querySelectorAll('.box')
 boxes.forEach(box => {
-	box.addEventListener('click', () => {
-		alert('Wyryto kliknięcie!')
+	box.addEventListener('click', e => {
+		alert('Kliknięto!')
 	})
 })
